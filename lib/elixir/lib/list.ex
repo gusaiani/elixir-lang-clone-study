d29ca668c502fda1c6012d6adcf80866df04a553
Gustavo Saiani <gustavo@poe.ma>
2016-11-23 18:03:10 -0200

    Add to List

--- lib/elixir/lib/list.ex
+++ lib/elixir/lib/list.ex
@@ -184,4 +184,491 @@ defmodule List do
   def foldl(list, acc, function) when is_list(list) and is_function(function) do
     :lists.foldl(function, acc, list)
   end
+
+  @doc """
+  Folds (reduces) the given list from the right with
+  a function. Requires an accumulator.
+
+  ## Examples
+
+      iex> List.foldr([1, 2, 3, 4], 0, fn(x, acc) -> x - acc end)
+      -2
+
+  """
+  @spec foldr([elem], acc, (elem, acc -> acc)) :: acc when elem: var, acc: var
+  def foldr(list, acc, function) when is_list(list) and is_function(function) do
+    :lists.foldr(function, acc, list)
+  end
+
+  @doc """
+  Returns the first element in `list` or `nil` if `list` is empty.
+
+  ## Examples
+
+      iex> List.first([])
+      nil
+
+      iex> List.first([1])
+      1
+
+      iex> List.first([1, 2, 3])
+      1
+
+  """
+  @spec first([elem]) :: nil | elem when elem: var
+  def first([]),      do: nil
+  def first([head | _]), do: head
+
+  @doc """
+  Returns the last element in `list` or `nil` if `list` is empty.
+
+  ## Examples
+
+      iex> List.last([])
+      nil
+
+      iex> List.last([1])
+      1
+
+      iex> List.last([1, 2, 3])
+      3
+
+  """
+  @spec last([elem]) :: nil | elem when elem: var
+  def last([]), do: nil
+  def last([head]), do: head
+  def last([_ | tail]), do: last(tail)
+
+  @doc """
+  Receives a list of tuples and returns the first tuple
+  where the item at `position` in the tuple matches the
+  given `key`.
+
+  ## Examples
+
+      iex> List.keyfind([a: 1, b: 2], :a, 0)
+      {:a, 1}
+
+      iex> List.keyfind([a: 1, b: 2], 2, 1)
+      {:b, 2}
+
+      iex> List.keyfind([a: 1, b: 2], :c, 0)
+      nil
+
+  """
+  @spec keyfind([tuple], any, non_neg_integer, any) :: any
+  def keyfind(list, key, position, default \\ nil) do
+    :lists.keyfind(key, position + 1, list) || default
+  end
+
+  @doc """
+  Receives a list of tuples and returns `true` if there is
+  a tuple where the item at `position` in the tuple matches
+  the given `key`.
+
+  ## Examples
+
+      iex> List.keymember?([a: 1, b: 2], :a, 0)
+      true
+
+      iex> List.keymember?([a: 1, b: 2], 2, 1)
+      true
+
+      iex> List.keymember?([a: 1, b: 2], :c, 0)
+      false
+
+  """
+  @spec keymember?([tuple], any, non_neg_integer) :: boolean
+  def keymember?(list, key, position) do
+    :lists.keymember(key, position + 1, list)
+  end
+
+  @doc """
+  Receives a list of tuples and replaces the item
+  identified by `key` at `position` if it exists.
+
+  ## Examples
+
+      iex> List.keyreplace([a: 1, b: 2], :a, 0, {:a, 3})
+      [a: 3, b: 2]
+
+  """
+  @spec keyreplace([tuple], any, non_neg_integer, tuple) :: [tuple]
+  def keyreplace(list, key, position, new_tuple) do
+    :lists.keyreplace(key, position + 1, list, new_tuple)
+  end
+
+  @doc """
+  Receives a list of tuples and sorts the items
+  at `position` of the tuples. The sort is stable.
+
+  ## Examples
+
+      iex> List.keysort([a: 5, b: 1, c: 3], 1)
+      [b: 1, c: 3, a: 5]
+
+      iex> List.keysort([a: 5, c: 1, b: 3], 0)
+      [a: 5, b: 3, c: 1]
+
+  """
+  @spec keysort([tuple], non_neg_integer) :: [tuple]
+  def keysort(list, position) do
+    :lists.keysort(position + 1, list)
+  end
+
+  @doc """
+  Receives a `list` of tuples and replaces the item
+  identified by `key` at `position`.
+
+  If the item does not exist, it is added to the end of the `list`.
+
+  ## Examples
+
+      iex> List.keystore([a: 1, b: 2], :a, 0, {:a, 3})
+      [a: 3, b: 2]
+
+      iex> List.keystore([a: 1, b: 2], :c, 0, {:c, 3})
+      [a: 1, b: 2, c: 3]
+
+  """
+  @spec keystore([tuple], any, non_neg_integer, tuple) :: [tuple, ...]
+  def keystore(list, key, position, new_tuple) do
+    :lists.keystore(key, position + 1, list, new_tuple)
+  end
+
+  @doc """
+  Receives a `list` of tuples and deletes the first tuple
+  where the item at `position` matches the
+  given `key`. Returns the new list.
+
+  ## Examples
+
+      iex> List.keydelete([a: 1, b: 2], :a, 0)
+      [b: 2]
+
+      iex> List.keydelete([a: 1, b: 2], 2, 1)
+      [a: 1]
+
+      iex> List.keydelete([a: 1, b: 2], :c, 0)
+      [a: 1, b: 2]
+
+  """
+  @spec keydelete([tuple], any, non_neg_integer) :: [tuple]
+  def keydelete(list, key, position) do
+    :lists.keydelete(key, position + 1, list)
+  end
+
+  @doc """
+  Receives a `list` of tuples and returns the first tuple
+  where the element at `position` in the tuple matches the
+  given `key`, as well as the `list` without found tuple.
+
+  If such a tuple is not found, `nil` will be returned.
+
+  ## Examples
+
+      iex> List.keytake([a: 1, b: 2], :a, 0)
+      {{:a, 1}, [b: 2]}
+
+      iex> List.keytake([a: 1, b: 2], 2, 1)
+      {{:b, 2}, [a: 1]}
+
+      iex> List.keytake([a: 1, b: 2], :c, 0)
+      nil
+
+  """
+  @spec keytake([tuple], any, non_neg_integer) :: {tuple, [tuple]} | nil
+  def keytake(list, key, position) do
+    case :lists.keytake(key, position + 1, list) do
+      {:value, item, list} -> {item, list}
+      false                -> nil
+    end
+  end
+
+  @doc """
+  Wraps the argument in a list.
+
+  If the argument is already a list, returns the list.
+  If the argument is `nil`, returns an empty list.
+
+  ## Examples
+
+      iex> List.wrap("hello")
+      ["hello"]
+
+      iex> List.wrap([1, 2, 3])
+      [1, 2, 3]
+
+      iex> List.wrap(nil)
+      []
+
+  """
+  @spec wrap(list | any) :: list
+  def wrap(list) when is_list(list) do
+    list
+  end
+
+  def wrap(nil) do
+    []
+  end
+
+  def wrap(other) do
+    [other]
+  end
+
+  @doc """
+  Zips corresponding elements from each list in `list_of_lists`.
+
+  The zipping finishes as soon as any list terminates.
+
+  ## Examples
+
+      iex> List.zip([[1, 2], [3, 4], [5, 6]])
+      [{1, 3, 5}, {2, 4, 6}]
+
+      iex> List.zip([[1, 2], [3], [5, 6]])
+      [{1, 3, 5}]
+
+  """
+  @spec zip([list]) :: [tuple]
+  def zip([]), do: []
+  def zip(list_of_lists) when is_list(list_of_lists) do
+    do_zip(list_of_lists, [])
+  end
+
+  @doc """
+  Returns a list with `value` inserted at the specified `index`.
+
+  Note that `index` is capped at the list length. Negative indices
+  indicate an offset from the end of the `list`.
+
+  ## Examples
+
+      iex> List.insert_at([1, 2, 3, 4], 2, 0)
+      [1, 2, 0, 3, 4]
+
+      iex> List.insert_at([1, 2, 3], 10, 0)
+      [1, 2, 3, 0]
+
+      iex> List.insert_at([1, 2, 3], -1, 0)
+      [1, 2, 3, 0]
+
+      iex> List.insert_at([1, 2, 3], -10, 0)
+      [0, 1, 2, 3]
+
+  """
+  @spec insert_at(list, integer, any) :: list
+  def insert_at(list, index, value) when is_integer(index) do
+    if index < 0 do
+      do_insert_at(list, length(list) + index + 1, value)
+    else
+      do_insert_at(list, index, value)
+    end
+  end
+
+  @doc """
+  Returns a list with a replaced value at the specified `index`.
+
+  Negative indices indicate an offset from the end of the `list`.
+  If `index` is out of bounds, the original `list` is returned.
+
+  ## Examples
+
+      iex> List.replace_at([1, 2, 3], 0, 0)
+      [0, 2, 3]
+
+      iex> List.replace_at([1, 2, 3], 10, 0)
+      [1, 2, 3]
+
+      iex> List.replace_at([1, 2, 3], -1, 0)
+      [1, 2, 0]
+
+      iex> List.replace_at([1, 2, 3], -10, 0)
+      [1, 2, 3]
+
+  """
+  @spec replace_at(list, integer, any) :: list
+  def replace_at(list, index, value) when is_integer(index) do
+    if index < 0 do
+      do_replace_at(list, length(list) + index, value)
+    else
+      do_replace_at(list, index, value)
+    end
+  end
+
+  @doc """
+  Returns a list with an updated value at the specified `index`.
+
+  Negative indices indicate an offset from the end of the `list`.
+  If `index` is out of bounds, the original `list` is returned.
+
+  ## Examples
+
+      iex> List.update_at([1, 2, 3], 0, &(&1 + 10))
+      [11, 2, 3]
+
+      iex> List.update_at([1, 2, 3], 10, &(&1 + 10))
+      [1, 2, 3]
+
+      iex> List.update_at([1, 2, 3], -1, &(&1 + 10))
+      [1, 2, 13]
+
+      iex> List.update_at([1, 2, 3], -10, &(&1 + 10))
+      [1, 2, 3]
+
+  """
+  @spec update_at([elem], integer, (elem -> any)) :: list when elem: var
+  def update_at(list, index, fun) when is_function(fun, 1) and is_integer(index) do
+    if index < 0 do
+      do_update_at(list, length(list) + index, fun)
+    else
+      do_update_at(list, index, fun)
+    end
+  end
+
+  @doc """
+  Produces a new list by removing the value at the specified `index`.
+
+  Negative indices indicate an offset from the end of the `list`.
+  If `index` is out of bounds, the original `list` is returned.
+
+  ## Examples
+
+      iex> List.delete_at([1, 2, 3], 0)
+      [2, 3]
+
+      iex> List.delete_at([1, 2, 3], 10)
+      [1, 2, 3]
+
+      iex> List.delete_at([1, 2, 3], -1)
+      [1, 2]
+
+  """
+  @spec delete_at(list, integer) :: list
+  def delete_at(list, index) when is_integer(index) do
+    elem(pop_at(list, index), 1)
+  end
+
+  @doc """
+  Returns and removes the value at the specified `index` in the `list`.
+
+  Negative indices indicate an offset from the end of the `list`.
+  If `index` is out of bounds, the original `list` is returned.
+
+  ## Examples
+
+      iex> List.pop_at([1, 2, 3], 0)
+      {1, [2, 3]}
+      iex> List.pop_at([1, 2, 3], 5)
+      {nil, [1, 2, 3]}
+      iex> List.pop_at([1, 2, 3], 5, 10)
+      {10, [1, 2, 3]}
+      iex> List.pop_at([1, 2, 3], -1)
+      {3, [1, 2]}
+
+  """
+  @spec pop_at(list, integer, any) :: {any, list}
+  def pop_at(list, index, default \\ nil) when is_integer(index) do
+    if index < 0 do
+      do_pop_at(list, length(list) + index, default, [])
+    else
+      do_pop_at(list, index, default, [])
+    end
+  end
+
+  ## Helpers
+
+  # replace_at
+
+  defp do_replace_at([], _index, _value) do
+    []
+  end
+
+  defp do_replace_at(list, index, _value) when index < 0 do
+    list
+  end
+
+  defp do_replace_at([_old | rest], 0, value) do
+    [value | rest]
+  end
+
+  defp do_replace_at([head | tail], index, value) do
+    [head | do_replace_at(tail, index - 1, value)]
+  end
+
+  # insert_at
+
+  defp do_insert_at([], _index, value) do
+    [value]
+  end
+
+  defp do_insert_at(list, index, value) when index <= 0 do
+    [value | list]
+  end
+
+  defp do_insert_at([head | tail], index, value) do
+    [head | do_insert_at(tail, index - 1, value)]
+  end
+
+  # update_at
+
+  defp do_update_at([value | list], 0, fun) do
+    [fun.(value) | list]
+  end
+
+  defp do_update_at(list, index, _fun) when index < 0 do
+    list
+  end
+
+  defp do_update_at([head | tail], index, fun) do
+    [head | do_update_at(tail, index - 1, fun)]
+  end
+
+  defp do_update_at([], _index, _fun) do
+    []
+  end
+
+  # pop_at
+
+  defp do_pop_at([], _index, default, acc) do
+    {default, :lists.reverse(acc)}
+  end
+
+  defp do_pop_at(list, index, default, []) when index < 0 do
+    {default, list}
+  end
+
+  defp do_pop_at([head | tail], 0, _default, acc) do
+    {head, :lists.reverse(acc, tail)}
+  end
+
+  defp do_pop_at([head | tail], index, default, acc) do
+    do_pop_at(tail, index - 1, default, [head | acc])
+  end
+
+  # zip
+
+  defp do_zip(list, acc) do
+    converter = fn x, acc -> do_zip_each(to_list(x), acc) end
+    case :lists.mapfoldl(converter, [], list) do
+      {_, nil} -> :lists.reverse(acc)
+      {mlist, heads} ->
+        do_zip(mlist, [to_tuple(:lists.reverse(heads)) | acc])
+    end
+  end
+
+  defp do_zip_each(_, nil) do
+    {nil, nil}
+  end
+
+  defp do_zip_each([head | tail], acc) do
+    {tail, [head | acc]}
+  end
+
+  defp do_zip_each([], _) do
+    {nil, nil}
+  end
+
+  defp to_list(tuple) when is_tuple(tuple), do: Tuple.to_list(tuple)
+  defp to_list(list)  when is_list(list),   do: list
 end
