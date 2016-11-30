04ff3812a8f25a4c7284a0d3facfae8a55e7d54e
Gustavo Saiani <gustavo@poe.ma>
2016-11-30 18:33:46 -0200

    Add to System

--- lib/elixir/lib/system.ex
+++ lib/elixir/lib/system.ex
@@ -24,7 +24,7 @@ defmodule System do
 
   If, while the code is executing, the system clock changes,
   some code that executed in 1 second may be reported as taking
-  over 1 hour! To address such concerts, the VM provides a
+  over 1 hour! To address such concerns, the VM provides a
   monotonic time via `System.monotonic_time/0` which never
   decreases and does not leap:
 
@@ -128,4 +128,188 @@ defmodule System do
     |> :os.cmd()
     |> strip
   end
+
+  defp revision, do: get_revision()
+
+  # Get the date at compilation time.
+  defmacrop get_date do
+    IO.iodata_to_binary :httpd_util.rfc1123_date
+  end
+
+  @doc """
+  Returns the endianness.
+  """
+  def endianness do
+    :erlang.system_info(:endian)
+  end
+
+  @doc """
+  Returns the endianness the system was compiled with.
+  """
+  @endianness :erlang.system_info(:endian)
+  def compiled_endianness do
+    @endianness
+  end
+
+  @doc """
+  Elixir version information.
+
+  Returns Elixir's version as binary.
+  """
+  @spec version() :: String.t
+  def version, do: get_version()
+
+  @doc """
+  Elixir build information.
+
+  Returns a keyword list with Elixir version, Git short revision hash and compilation date.
+  """
+  @spec build_info() :: map
+  def build_info do
+    %{build:    build(),
+      date:     get_date(),
+      revision: revision(),
+      version:  version()}
+  end
+
+  # Returns a string of the build info
+  defp build do
+    {:ok, v} = Version.parse(version())
+
+    cond do
+      ([] == v.pre) or ("" == revision()) ->
+        version()
+      true ->
+        "#{version()} (#{revision()})"
+    end
+  end
+
+  @doc """
+  Lists command line arguments.
+
+  Returns the list of command line arguments passed to the program.
+  """
+  @spec argv() :: [String.t]
+  def argv do
+    :elixir_config.get(:argv)
+  end
+
+  @doc """
+  Modifies command line arguments.
+
+  Changes the list of command line arguments. Use it with caution,
+  as it destroys any previous argv information.
+  """
+  @spec argv([String.t]) :: :ok
+  def argv(args) do
+    :elixir_config.put(:argv, args)
+  end
+
+  @doc """
+  Current working directory.
+
+  Returns the current working directory or `nil` if one
+  is not available.
+  """
+  def cwd do
+    case :file.get_cwd do
+      {:ok, base} -> IO.chardata_to_string(fix_drive_letter(base))
+      _ -> nil
+    end
+  end
+
+  defp fix_drive_letter([l, ?:, ?/ | rest] = original) when l in ?A..?Z do
+    case :os.type() do
+      {:win32, _} -> [l+?a-?A, ?:, ?/ | rest]
+      _ -> original
+    end
+  end
+
+  defp fix_drive_letter(original), do: original
+
+  @doc """
+  Current working directory, exception on error.
+
+  Returns the current working directory or raises `RuntimeError`.
+  """
+  def cwd! do
+    cwd() ||
+      raise RuntimeError, message: "could not get a working directory, the current location is not accessible"
+  end
+
+  @doc """
+  User home directory.
+
+  Returns the user home directory (platform independent).
+  """
+  def user_home do
+    :elixir_config.get(:home)
+  end
+
+  @doc """
+  User home directory, exception on error.
+
+  Same as `user_home/0` but raises `RuntimeError`
+  instead of returning `nil` if no user home is set.
+  """
+  def user_home! do
+    user_home() ||
+      raise RuntimeError, message: "could not find the user home, please set the HOME environment variable"
+  end
+
+  @doc ~S"""
+  Writable temporary directory.
+
+  Returns a writable temporary directory.
+  Searches for directories in the following order:
+
+    1. the directory named by the TMPDIR environment variable
+    2. the directory named by the TEMP environment variable
+    3. the directory name by the TMP environment variable
+    4. `C:\TMP` on Window or `/tmp` on Unix
+    5. as a last resort, the current working directory
+
+  Returns `nil` if one of the above are writable.
+  """
+  def tmp_dir do
+    write_env_tmp_dir('TMPDIR') ||
+      write_env_tmp_dir('TEMP') ||
+      write_env_tmp_dir('TMP')  ||
+      write_tmp_dir('/tmp')     ||
+      ((cwd = cwd()) && write_tmp_dir(cwd))
+  end
+
+  @doc """
+  Writable temporary directorty, exception on error.
+
+  Same as `tmp_dir/0` but raises `RuntimeError`
+  instead of returning `nil` if no temp dir is set.
+  """
+  def tmp_dir! do
+    tmp_dir() ||
+      raise RuntimeError, message: "could not get a writable temporary directory, " <>
+                                   "please set the TMPDIR environment variable"
+  end
+
+  defp write_env_tmp_dir(env) do
+    case :os.getenv(env) do
+      false -> nil
+      tmp   -> write_tmp_dir(tmp)
+    end
+  end
+
+  defp write_tmp_dir(dir) do
+    case File.stat(dir) do
+      {:ok, stat} ->
+        case {stat.type, stat.access} do
+          {:directory, access} when access in [:read_write, :write] ->
+            IO.chardata_to_string(dir)
+          _ ->
+            nil
+        end
+      {:error, _} -> nil
+    end
+  end
+
+  
 end
