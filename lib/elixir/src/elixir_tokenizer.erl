9ba54f0c6ab7927dabe84b1a9cffbd1799879ed0
Gustavo Saiani <gustavo@poe.ma>
2017-07-26 16:13:06 +0200

    Add to elixir_tokenizer

--- lib/elixir/src/elixir_tokenizer.erl
+++ lib/elixir/src/elixir_tokenizer.erl
@@ -2,3 +2,65 @@
 -include("elixir.hrl").
 -export([tokenize/1, tokenize/3, tokenize/4, invalid_do_error/1]).
 -import(elixir_interpolation, [unescape_tokens/1]).
+
+%% Numbers
+-define(is_hex(S), (?is_digit(S) orelse (S >= $A andalso S =< $F) orelse (S >= $a andalso S =< $f))).
+-define(is_bin(S), (S >= $0 andalso S =< $1)).
+-define(is_octal(S), (S >= $0 andalso S =< $7)).
+
+%% Digits and letters
+-define(is_digit(S), (S >= $0 andalso S =< $9)).
+-define(is_upcase(S), (S >= $A andalso S =< $Z)).
+-define(is_downcase(S), (S >= $a andalso S =< $z)).
+
+%% Others
+-define(is_quote(S), (S == $" orelse S == $')).
+-define(is_sigil(S), ((S == $/) orelse (S == $<) orelse (S == $") orelse (S == $') orelse
+                      (S == $[) orelse (S == $() orelse (S == ${) orelse (S == $|))).
+
+%% Spaces
+-define(is_horizontal_space(S), ((S == $\s) orelse (S == $\t))).
+-define(is_vertical_space(S), ((S == $\r) orelse (S == $\n))).
+-define(is_space(S), (?is_horizontal_space(S) orelse ?is_vertical_space(S))).
+
+%% Operators
+-define(at_op(T),
+  T == $@).
+
+-define(capture_op(T),
+  T == $&).
+
+-define(unary_op(T),
+  T == $!;
+  T == $^).
+
+-define(unary_op3(T1, T2, T3),
+  T1 == $~, T2 == $~, T3 == $~).
+
+-define(two_op(T1, T2),
+  T1 == $+, T2 == $+;
+  T1 == $-, T2 == $-;
+  T1 == $<, T2 == $>;
+  T1 == $., T2 == $.).
+
+-define(three_op(T1, T2, T3),
+  T1 == $^, T2 == $^, T3 == $^).
+
+-define(mult_op(T),
+  T == $* orelse T == $/).
+
+-define(dual_op(T),
+  T == $+ orelse T == $-).
+
+-define(arrow_op3(T1, T2, T3),
+  T1 == $<, T2 == $<, T3 == $<;
+  T1 == $>, T2 == $>, T3 == $>;
+  T1 == $~, T2 == $>, T3 == $>;
+  T1 == $<, T2 == $<, T3 == $~;
+  T1 == $<, T2 == $~, T3 == $>;
+  T1 == $<, T2 == $|, T3 == $>).
+
+-define(arrow_op(T1, T2),
+  T1 == $|, T2 == $>;
+  T1 == $~, T2 == $>;
+  T1 == $<, T2 == $~;).
