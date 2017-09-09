fd1de15d2667e54f1e4b1d069fed501a28fe4f3e
Gustavo Saiani <gustavo@poe.ma>
2017-09-09 11:37:29 +0200

    More Mix.Utils

--- lib/mix/lib/mix/utils.ex
+++ lib/mix/lib/mix/utils.ex
@@ -64,4 +64,105 @@ defmodule Mix.Utils do
       iex> Mix.Utils.parse_mfa("Foo.bar/2/2")
       :error
   """
+  def parse_mfa(mfa) do
+    with {:ok, quoted} <- Code.string_to_quoted(mfa),
+         [_ | _] = mfa_list <- quoted_to_mfa(quoted) do
+      {:ok, mfa_list}
+    else
+      _ -> :error
+    end
+  end
 
+  defp quoted_to_mfa({:/, _, [dispatch, arity]}) when is_integer(arity) do
+    quoted_to_mf(dispatch, [arity])
+  end
+  defp quoted_to_mfa(dispatch) do
+    quoted_to_mf(dispatch, [])
+  end
+
+  defp quoted_to_mf({{:., _, [module, fun]}, _, []}, acc) when is_atom(fun) do
+    quoted_to_m(module, [fun | acc])
+  end
+  defp quoted_to_mf(module, acc) do
+    quoted_to_m(module, acc)
+  end
+
+  defp quoted_to_m({:__aliases__, _, aliases}, acc) do
+    [Module.concat(aliases) | acc]
+  end
+  defp quoted_to_m(atom, acc) when is_atom(atom) do
+    [atom | acc]
+  end
+  defp quoted_to_m(_, _acc) do
+    []
+  end
+
+  @doc """
+  Takes a `command` name and attempts to load a module
+  with the command name converted to a module name
+  in the given `at` scope.
+
+  Returns `{:module, module}` in case a module
+  exists and is loaded, `{:error, reason}` otherwise.
+
+  ## Examples
+
+      iex> Mix.Utils.command_to_module("compile", Mix.Tasks)
+      {:module, Mix.Tasks.Compile}
+
+  """
+  def command_to_module(command, at \\ Elixir) do
+    module = Module.concat(at, command_to_module_name(command))
+    Code.ensure_loaded(module)
+  end
+
+  @doc """
+  Returns `true` if any of the `sources` are stale
+  compared to the given `targets`.
+  """
+  def stale?(sources, targets) do
+    Enum.any? stale_stream(sources, targets)
+  end
+
+  @doc """
+  Extracts all stale `sources` compared to the given `targets`.
+  """
+  def extract_stale(_sources, []), do: []
+  def extract_stale([], _targets), do: []
+
+  def extract_stale(sources, targets) do
+    stale_stream(sources, targets) |> Enum.to_list
+  end
+
+  defp stale_stream(sources, targets) do
+    modified_target = targets |> Enum.map(&last_modified/1) |> Enum.min
+
+    Stream.filter(sources, fn(source) ->
+      last_modified(source) > modified_target
+    end)
+  end
+
+  @doc """
+  Returns the date the given path was last modified.
+
+  If the path does not exist, it returns the Unix epoch
+  (1970-01-01 00:00:00).
+  """
+  def last_modified(path)
+
+  def last_modified({{_, _, _}, {_, _, _}} = timestamp) do
+    timestamp
+  end
+
+  def last_modified(path) do
+    {mtime, _size} = last_modified_and_size(path)
+    mtime
+  end
+
+  @doc false
+  def last_modified_and_size(path) do
+    now = :calendar.universal_time
+
+    case :elixir_utils.read_mtime_and_size(path) do
+      {:ok, mtime, size} when mtime > now ->
+        Mix.shell.error("warning: mtime (modified time) for \"#{path}\" was set to the future, resetting to now")
