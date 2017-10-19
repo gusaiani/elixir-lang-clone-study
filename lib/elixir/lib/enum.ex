eb0a2dc1f22af452c0da721d1a9837475da7887d
Gustavo Saiani <gustavo@poe.ma>
2017-10-19 13:36:44 -0200

    More of Enum

--- lib/elixir/lib/enum.ex
+++ lib/elixir/lib/enum.ex
@@ -81,4 +81,151 @@ defprotocol Enumerable do
           {:done, term}
           | {:halted, term}
           | {:suspended, term, continuation}
+
+  @typedoc """
+  A partially applied reduce function.
+
+  The continuation is the closure returned as a result when
+  the enumeration is suspended. When invoked, it expects
+  a new accumulator and it returns the result.
+
+  A continuation is easily implemented as long as the reduce
+  function is defined in a tail recursive fashion. If the function
+  is tail recursive, all the state is passed as arguments, so
+  the continuation would simply be the reducing function partially
+  applied.
+  """
+  @type continuation :: (acc -> result)
+
+  @doc """
+  Reduces the enumerable into an element.
+
+  Most of the operations in `Enum` are implemented in terms of reduce.
+  This function should apply the given `t:reducer/0` function to each
+  item in the enumerable and proceed as expected by the returned
+  accumulator.
+
+  As an example, here is the implementation of `reduce` for lists:
+
+      def reduce(_,       {:halt, acc}, _fun),   do: {:halted, acc}
+      def reduce(list,    {:suspend, acc}, fun), do: {:suspended, acc, &reduce(list, &1, fun)}
+      def reduce([],      {:cont, acc}, _fun),   do: {:done, acc}
+      def reduce([h | t], {:cont, acc}, fun),    do: reduce(t, fun.(h, acc), fun)
+
+  """
+  @spec reduce(t, acc, reducer) :: result
+  def reduce(enumerable, acc, fun)
+
+  @doc """
+  Checks if an element exists within the enumerable.
+
+  It should return `{:ok, boolean}`.
+
+  If `{:error, __MODULE__}` is returned a default algorithm using
+  `reduce` and the match (`===`) operator is used. This algorithm runs
+  in linear time.
+
+  Please force use of the default algorithm unless you can implement an
+  algorithm that is significantly faster.
+  """
+  @spec member?(t, term) :: {:ok, boolean} | {:error, module}
+  def member?(enumerable, element)
+
+  @doc """
+  Retrieves the enumerable's size.
+
+  It should return `{:ok, size}`.
+
+  If `{:error, __MODULE__}` is returned a default algorithm using
+  `reduce` is used. This algorithm runs in linear time.
+
+  Please force use of the default algorithm unless you can implement an
+  algorithm that is significantly faster.
+  """
+  @spec count(t) :: {:ok, non_neg_integer} | {:error, module}
+  def count(enumerable)
+end
+
+defmodule Enum do
+  import Kernel, except: [max: 2, min: 2]
+
+  @moduledoc """
+  Provides a set of algorithms that enumerate over enumerables according
+  to the `Enumerable` protocol.
+
+      iex> Enum.map([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  Some particular types, like maps, yield a specific format on enumeration.
+  For example, the argument is always a `{key, value}` tuple for maps:
+
+      iex> map = %{a: 1, b: 2}
+      iex> Enum.map(map, fn {k, v} -> {k, v * 2} end)
+      [a: 2, b: 4]
+
+  Note that the functions in the `Enum` module are eager: they always
+  start the enumeration of the given enumerable. The `Stream` module
+  allows lazy enumeration of enumerables and provides infinite streams.
+
+  Since the majority of the functions in `Enum` enumerate the whole
+  enumerable and return a list as result, infinite streams need to
+  be carefully used with such functions, as they can potentially run
+  forever. For example:
+
+      Enum.each Stream.cycle([1, 2, 3]), &IO.puts(&1)
+
+  """
+
+  @compile :inline_list_funcs
+
+  @type t :: Enumerable.t()
+  @type acc :: any
+  @type element :: any
+  @type index :: integer
+  @type default :: any
+
+  # Require Stream.Reducers and its callbacks
+  require Stream.Reducers, as: R
+
+  defmacrop skip(acc) do
+    acc
+  end
+
+  defmacrop next(_, entry, acc) do
+    quote(do: [unquote(entry) | unquote(acc)])
+  end
+
+  defmacrop acc(head, state, _) do
+    quote(do: {unquote(head), unquote(state)})
+  end
+
+  defmacrop next_with_acc(_, entry, head, state, _) do
+    quote do
+      {[unquote(entry) | unquote(head)], unquote(state)}
+    end
+  end
+
+  @doc """
+  Returns true if the given `fun` evaluates to true on all of the items in the enumerable.
+
+  It stops the iteration at the first invocation that returns `false` or `nil`.
+
+  ## Examples
+
+      iex> Enum.all?([2, 4, 6], fn(x) -> rem(x, 2) == 0 end)
+      true
+
+      iex> Enum.all?([2, 3, 4], fn(x) -> rem(x, 2) == 0 end)
+      false
+
+  If no function is given, it defaults to checking if
+  all items in the enumerable are truthy values.
+
+      iex> Enum.all?([1, 2, 3])
+      true
+
+      iex> Enum.all?([1, nil, 3])
+      false
+
+  """
 end
