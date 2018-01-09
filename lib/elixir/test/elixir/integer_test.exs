45e8291e82ba1af300907013f513a1d8d9077979
Gustavo Saiani <gustavo@poe.ma>
2018-01-09 11:05:00 -0200

    More of Integer test

--- lib/elixir/test/elixir/integer_test.exs
+++ lib/elixir/test/elixir/integer_test.exs
@@ -41,7 +41,7 @@ defmodule IntegerTest do
     assert test_is_even_in_guards(:not_integer) == :atom
   end
 
-  test "mod/1" do
+  test "mod/2" do
     assert Integer.mod(3, 2) == 1
     assert Integer.mod(0, 10) == 0
     assert Integer.mod(30000, 2001) == 1986
@@ -57,4 +57,44 @@ defmodule IntegerTest do
     assert_raise ArithmeticError, fn -> Integer.mod(3.0, 2) end
     assert_raise ArithmeticError, fn -> Integer.mod(20, 1.2) end
   end
+
+  test "floor_div/2" do
+    assert Integer.floor_div(3, 2) == 1
+    assert Integer.floor_div(0, 10) == 0
+    assert Integer.floor_div(30000, 2001) == 14
+    assert Integer.floor_div(-20, 11) == -2
+  end
+
+  test "floor_div/2 raises ArithmeticError when divisor is 0" do
+    assert_raise ArithmeticError, fn -> Integer.floor_div(3, 0) end
+    assert_raise ArithmeticError, fn -> Integer.floor_div(-50, 0) end
+  end
+
+  test "floor_div/2 raises ArithmeticError when non-integers used as arguments" do
+    assert_raise ArithmeticError, fn -> Integer.floor_div(3.0, 2) end
+    assert_raise ArithmeticError, fn -> Integer.floor_div(20, 1.2) end
+  end
+
+  test "digits/2" do
+    assert Integer.digits(0) == [0]
+    assert Integer.digits(0, 2) == [0]
+    assert Integer.digits(1) == [1]
+    assert Integer.digits(-1) == [-1]
+    assert Integer.digits(123, 123) == [1, 0]
+    assert Integer.digits(-123, 123) == [-1, 0]
+    assert Integer.digits(456, 1000) == [456]
+    assert Integer.digits(-456, 1000) == [-456]
+    assert Integer.digits(123) == [1, 2, 3]
+    assert Integer.digits(-123) == [-1, -2, -3]
+    assert Integer.digits(58127, 2) == [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1]
+    assert Integer.digits(-58127, 2) == [-1, -1, -1, 0, 0, 0, -1, -1, 0, 0, 0, 0, -1, -1, -1, -1]
+
+    for n <- Enum.to_list(-1..1) do
+      assert_raise FunctionClauseError, fn ->
+        Integer.digits(10, n)
+        Integer.digits(-10, n)
+      end
+    end
+  end
+
 end
