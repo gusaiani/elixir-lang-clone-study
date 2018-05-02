727f8ccc24ed5a132d98bd37853ea2d387690df3
Gustavo Saiani <gustavo@poe.ma>
2018-05-02 11:27:46 +0200

    More of Task.Compiler

--- lib/mix/lib/mix/task.compiler.ex
+++ lib/mix/lib/mix/task.compiler.ex
@@ -20,7 +20,7 @@ defmodule Mix.Task.Compiler do
   editors to display issues inline without having to analyze the
   command-line output.
 
-  If the compiler uses manifest files to track stale sources, is should
+  If the compiler uses manifest files to track stale sources, it should
   define `manifests/0`, and if it writes any output to disk it should
   also define `clean/0`.
 
@@ -51,7 +51,7 @@ defmodule Mix.Task.Compiler do
         programmer may have made a mistake
 
       * `:hint` - A suggestion for style or good practices that is not as
-        sevre as a warning
+        severe as a warning
 
       * `:information` - Any other information relevant to compilation that
         does not fit into the above categories
@@ -60,7 +60,7 @@ defmodule Mix.Task.Compiler do
     @type severity :: :error | :warning | :information | :hint
 
     @typedoc """
-    Where in a file the diagnostic applies. Can be einther a line number,
+    Where in a file the diagnostic applies. Can be either a line number,
     a range specified as `{start_line, start_col, end_line, end_col}`,
     or `nil` if unknown.
 
@@ -69,3 +69,48 @@ defmodule Mix.Task.Compiler do
     to indicate that a diagnostic applies to the first `n` characters of the
     first line, the range would be `{1, 0, 1, n}`.
     """
+    @type position ::
+            nil
+            | pos_integer
+            | {pos_integer, non_neg_integer, pos_integer, non_neg_integer}
+
+    @enforce_keys [:file, :severity, :message, :position, :compiler_name]
+    defstruct [:file, :severity, :message, :position, :compiler_name, :details]
+  end
+
+  @doc """
+  Receives command-line arguments and performs compilation. If it
+  produces errors, warnings, or any other diagnostic information,
+  it should return a tuple with the status and a list of diagnostics.
+  """
+  @callback run([binary]) ::
+              :ok
+              | :noop
+              | {:ok | :noop | :error, [Diagnostic.t()]}
+
+  @doc """
+  Lists manifest files for the compiler.
+  """
+  @callback manifests() :: [Path.t()]
+
+  @doc """
+  Removes build artifacts and manifests.
+  """
+  @callback clean() :: any
+
+  @optional_callbacks clean: 0, manifests: 0
+
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+      Enum.each(
+        Mix.Task.supported_attributes(),
+        &Module.register_attribute(__MODULE__, &1, persist: true)
+      )
+
+      @behaviour Mix.Task.Compiler
+    end
+  end
+
+  # Normalize the compiler result to a diagnostic tuple.
+  @doc false  q
