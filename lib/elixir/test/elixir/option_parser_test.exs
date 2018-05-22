2894a09207853acc636f9d4a64aa012bf10e328d
Gustavo Saiani <gustavo@poe.ma>
2018-05-22 15:09:31 +0200

    More of OptionParserTest

--- lib/elixir/test/elixir/option_parser_test.exs
+++ lib/elixir/test/elixir/option_parser_test.exs
@@ -84,3 +84,269 @@ defmodule OptionParserTest do
       OptionParser.parse(["--elixir"], switches: [ex: :string], strict: [elixir: :string])
     end
   end
+
+  test "parse!/2 raises an exception for an unknown option using strict" do
+    assert_raise OptionParser.ParseError, "1 error found!\n--doc : Unknown option", fn ->
+      argv = ["--source", "from_docs/", "--doc", "show"]
+      OptionParser.parse!(argv, strict: [source: :string, docs: :string])
+    end
+  end
+
+  test "parse!/2 raises an exception when an option is of the wrong type" do
+    assert_raise OptionParser.ParseError, fn ->
+      argv = ["--bad", "opt", "foo", "-o", "bad", "bar"]
+      OptionParser.parse!(argv, switches: [bad: :integer])
+    end
+  end
+
+  test "parse_head!/2 raises an exception when an option is of the wrong type" do
+    message = "1 error found!\n--number : Expected type integer, got \"lib\""
+
+    assert_raise OptionParser.ParseError, message, fn ->
+      argv = ["--number", "lib", "test/enum_test.exs"]
+      OptionParser.parse_head!(argv, strict: [number: :integer])
+    end
+  end
+
+  describe "arguments" do
+    test "parses until --" do
+      assert OptionParser.parse(
+               ["--source", "foo", "--", "1", "2", "3"],
+               switches: [source: :string]
+             ) == {[source: "foo"], ["1", "2", "3"], []}
+
+      assert OptionParser.parse_head(
+               ["--source", "foo", "--", "1", "2", "3"],
+               switches: [source: :string]
+             ) == {[source: "foo"], ["1", "2", "3"], []}
+
+      assert OptionParser.parse(
+               ["--source", "foo", "bar", "--", "-x"],
+               switches: [source: :string]
+             ) == {[source: "foo"], ["bar", "-x"], []}
+
+      assert OptionParser.parse_head(
+               ["--source", "foo", "bar", "--", "-x"],
+               switches: [source: :string]
+             ) == {[source: "foo"], ["bar", "--", "-x"], []}
+    end
+
+    test "parses - as argument" do
+      argv = ["--foo", "-", "-b", "-"]
+      opts = [strict: [foo: :boolean, boo: :string], aliases: [b: :boo]]
+      assert OptionParser.parse(argv, opts) == {[foo: true, boo: "-"], ["-"], []}
+    end
+
+    test "parses until first non-option arguments" do
+      argv = ["--source", "from_docs/", "test/enum_test.exs", "--verbose"]
+
+      assert OptionParser.parse_head(argv, switches: [source: :string]) ==
+               {[source: "from_docs/"], ["test/enum_test.exs", "--verbose"], []}
+    end
+  end
+
+  describe "aliases" do
+    test "supports boolean aliases" do
+      assert OptionParser.parse(["-d"], aliases: [d: :docs], switches: [docs: :boolean]) ==
+               {[docs: true], [], []}
+    end
+
+    test "supports non-boolean aliases" do
+      assert OptionParser.parse(
+               ["-s", "from_docs/"],
+               aliases: [s: :source],
+               switches: [source: :string]
+             ) == {[source: "from_docs/"], [], []}
+    end
+
+    test "supports --key=value aliases" do
+      assert OptionParser.parse(
+               ["-s=from_docs/", "other"],
+               aliases: [s: :source],
+               switches: [source: :string]
+             ) == {[source: "from_docs/"], ["other"], []}
+    end
+
+    test "parses -ab as -a -b" do
+      aliases = [a: :first, b: :second]
+
+      assert OptionParser.parse(["-ab=1"], aliases: aliases, switches: [second: :integer]) ==
+               {[first: true, second: 1], [], []}
+
+      assert OptionParser.parse(["-ab", "1"], aliases: aliases, switches: [second: :integer]) ==
+               {[first: true, second: 1], [], []}
+    end
+  end
+
+  describe "types" do
+    test "parses configured booleans" do
+      assert OptionParser.parse(["--docs=false"], switches: [docs: :boolean]) ==
+               {[docs: false], [], []}
+
+      assert OptionParser.parse(["--docs=true"], switches: [docs: :boolean]) ==
+               {[docs: true], [], []}
+
+      assert OptionParser.parse(["--docs=other"], switches: [docs: :boolean]) ==
+               {[], [], [{"--docs", "other"}]}
+
+      assert OptionParser.parse(["--docs="], switches: [docs: :boolean]) ==
+               {[], [], [{"--docs", ""}]}
+
+      assert OptionParser.parse(["--docs", "foo"], switches: [docs: :boolean]) ==
+               {[docs: true], ["foo"], []}
+
+      assert OptionParser.parse(["--no-docs", "foo"], switches: [docs: :boolean]) ==
+               {[docs: false], ["foo"], []}
+
+      assert OptionParser.parse(["--no-docs=foo", "bar"], switches: [docs: :boolean]) ==
+               {[], ["bar"], [{"--no-docs", "foo"}]}
+
+      assert OptionParser.parse(["--no-docs=", "bar"], switches: [docs: :boolean]) ==
+               {[], ["bar"], [{"--no-docs", ""}]}
+    end
+
+    test "does not set unparsed booleans" do
+      assert OptionParser.parse(["foo"], switches: [docs: :boolean]) == {[], ["foo"], []}
+    end
+
+    test "keeps options on configured keep" do
+      argv = ["--require", "foo", "--require", "bar", "baz"]
+
+      assert OptionParser.parse(argv, switches: [require: :keep]) ==
+               {[require: "foo", require: "bar"], ["baz"], []}
+
+      assert OptionParser.parse(["--require"], switches: [require: :keep]) ==
+               {[], [], [{"--require", nil}]}
+    end
+
+    test "parses configured strings" do
+      assert OptionParser.parse(["--value", "1", "foo"], switches: [value: :string]) ==
+               {[value: "1"], ["foo"], []}
+
+      assert OptionParser.parse(["--value=1", "foo"], switches: [value: :string]) ==
+               {[value: "1"], ["foo"], []}
+
+      assert OptionParser.parse(["--value"], switches: [value: :string]) ==
+               {[], [], [{"--value", nil}]}
+
+      assert OptionParser.parse(["--no-value"], switches: [value: :string]) ==
+               {[no_value: true], [], []}
+    end
+
+    test "parses configured counters" do
+      assert OptionParser.parse(["--verbose"], switches: [verbose: :count]) ==
+               {[verbose: 1], [], []}
+
+      assert OptionParser.parse(["--verbose", "--verbose"], switches: [verbose: :count]) ==
+               {[verbose: 2], [], []}
+
+      argv = ["--verbose", "-v", "-v", "--", "bar"]
+      opts = [aliases: [v: :verbose], strict: [verbose: :count]]
+      assert OptionParser.parse(argv, opts) == {[verbose: 3], ["bar"], []}
+    end
+
+    test "parses configured integers" do
+      assert OptionParser.parse(["--value", "1", "foo"], switches: [value: :integer]) ==
+               {[value: 1], ["foo"], []}
+
+      assert OptionParser.parse(["--value=1", "foo"], switches: [value: :integer]) ==
+               {[value: 1], ["foo"], []}
+
+      assert OptionParser.parse(["--value", "WAT", "foo"], switches: [value: :integer]) ==
+               {[], ["foo"], [{"--value", "WAT"}]}
+    end
+
+    test "parses configured integers with keep" do
+      argv = ["--value", "1", "--value", "2", "foo"]
+
+      assert OptionParser.parse(argv, switches: [value: [:integer, :keep]]) ==
+               {[value: 1, value: 2], ["foo"], []}
+
+      argv = ["--value=1", "foo", "--value=2", "bar"]
+
+      assert OptionParser.parse(argv, switches: [value: [:integer, :keep]]) ==
+               {[value: 1, value: 2], ["foo", "bar"], []}
+    end
+
+    test "parses configured floats" do
+      assert OptionParser.parse(["--value", "1.0", "foo"], switches: [value: :float]) ==
+               {[value: 1.0], ["foo"], []}
+
+      assert OptionParser.parse(["--value=1.0", "foo"], switches: [value: :float]) ==
+               {[value: 1.0], ["foo"], []}
+
+      assert OptionParser.parse(["--value", "WAT", "foo"], switches: [value: :float]) ==
+               {[], ["foo"], [{"--value", "WAT"}]}
+    end
+
+    test "correctly handles negative integers" do
+      opts = [switches: [option: :integer], aliases: [o: :option]]
+      assert OptionParser.parse(["arg1", "-o", "-43"], opts) == {[option: -43], ["arg1"], []}
+
+      assert OptionParser.parse(["arg1", "--option=-43"], switches: [option: :integer]) ==
+               {[option: -43], ["arg1"], []}
+    end
+
+    test "correctly handles negative floating-point numbers" do
+      opts = [switches: [option: :float], aliases: [o: :option]]
+      assert OptionParser.parse(["arg1", "-o", "-43.2"], opts) == {[option: -43.2], ["arg1"], []}
+
+      assert OptionParser.parse(["arg1", "--option=-43.2"], switches: [option: :float]) ==
+               {[option: -43.2], ["arg1"], []}
+    end
+  end
+
+  describe "next" do
+    test "with strict good options" do
+      config = [strict: [str: :string, int: :integer, bool: :boolean]]
+      assert OptionParser.next(["--str", "hello", "..."], config) == {:ok, :str, "hello", ["..."]}
+      assert OptionParser.next(["--int=13", "..."], config) == {:ok, :int, 13, ["..."]}
+      assert OptionParser.next(["--bool=false", "..."], config) == {:ok, :bool, false, ["..."]}
+      assert OptionParser.next(["--no-bool", "..."], config) == {:ok, :bool, false, ["..."]}
+      assert OptionParser.next(["--bool", "..."], config) == {:ok, :bool, true, ["..."]}
+      assert OptionParser.next(["..."], config) == {:error, ["..."]}
+    end
+
+    test "with strict unknown options" do
+      config = [strict: [bool: :boolean]]
+
+      assert OptionParser.next(["--str", "13", "..."], config) ==
+               {:undefined, "--str", nil, ["13", "..."]}
+
+      assert OptionParser.next(["--int=hello", "..."], config) ==
+               {:undefined, "--int", "hello", ["..."]}
+
+      assert OptionParser.next(["-no-bool=other", "..."], config) ==
+               {:undefined, "-no-bool", "other", ["..."]}
+    end
+
+    test "with strict bad type" do
+      config = [strict: [str: :string, int: :integer, bool: :boolean]]
+      assert OptionParser.next(["--str", "13", "..."], config) == {:ok, :str, "13", ["..."]}
+
+      assert OptionParser.next(["--int=hello", "..."], config) ==
+               {:invalid, "--int", "hello", ["..."]}
+
+      assert OptionParser.next(["--int", "hello", "..."], config) ==
+               {:invalid, "--int", "hello", ["..."]}
+
+      assert OptionParser.next(["--bool=other", "..."], config) ==
+               {:invalid, "--bool", "other", ["..."]}
+    end
+
+    test "with strict missing value" do
+      config = [strict: [str: :string, int: :integer, bool: :boolean]]
+      assert OptionParser.next(["--str"], config) == {:invalid, "--str", nil, []}
+      assert OptionParser.next(["--int"], config) == {:invalid, "--int", nil, []}
+      assert OptionParser.next(["--bool=", "..."], config) == {:invalid, "--bool", "", ["..."]}
+
+      assert OptionParser.next(["--no-bool=", "..."], config) ==
+               {:invalid, "--no-bool", "", ["..."]}
+    end
+  end
+
+  test "split" do
+    assert OptionParser.split(~S[]) == []
+    assert OptionParser.split(~S[foo]) == ["foo"]
+    assert OptionParser.split(~S[foo bar]) == ["foo", "bar"]
+    assert OptionParser.split(~S[  foo  bar   ]) == ["foo", "bar"]
