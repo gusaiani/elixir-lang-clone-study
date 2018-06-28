7fdee37fa74d3b67b2b525c75052429f481c0a57
Gustavo Saiani <gustavo@poe.ma>
2018-06-28 09:30:13 -0300

    More of Regex

--- lib/elixir/lib/regex.ex
+++ lib/elixir/lib/regex.ex
@@ -104,4 +104,83 @@ defmodule Regex do
     * `list(binary)` - a list of named captures to capture
 
   """
+
+  defstruct re_pattern: nil, source: "", opts: "", re_version: ""
+
+  @type t :: %__MODULE__{re_pattern: term, source: binary, opts: binary}
+
+  defmodule CompileError do
+    defexception message: "regex could not be compiled"
+  end
+
+  @doc """
+  Compiles the regular expression.
+
+  The given options can either be a binary with the characters
+  representing the same regex options given to the
+  [`~r`](`Kernel.sigil_r/2`) sigil, or a list of options, as
+  expected by the Erlang's `:re` module.
+
+  It returns `{:ok, regex}` in case of success,
+  `{:error, reason}` otherwise.
+
+  ## Examples
+
+      iex> Regex.compile("foo")
+      {:ok, ~r/foo/}
+
+      iex> Regex.compile("*foo")
+      {:error, {'nothing to repeat', 0}}
+
+  """
+  @spec compile(binary, binary | [term]) :: {:ok, t} | {:error, any}
+  def compile(source, options \\ "") do
+    compile(source, options, version())
+  end
+
+  defp compile(source, options, version) when is_binary(options) do
+    case translate_options(options, []) do
+      {:error, rest} ->
+        {:error, {:invalid_option, rest}}
+
+      translated_options ->
+        compile(source, translated_options, options, version)
+    end
+  end
+
+  @doc """
+  Returns the version of the underlying Regex engine.
+  """
+  @since "1.4.0"
+  @spec version :: term()
+  # TODO: No longer check for function_exported? on OTP 20+.
+  def version do
+    if function_exported?(:re, :version, 0) do
+      {:re.version(), :erlang.system_info(:endian)}
+    else
+      {"8.33 2013-05-29", :erlang.system_info(:endian)}
+    end
+  end
+
+  # Private Helpers
+
+  defp translate_options(<<?u, t::binary>>, acc), do: translate_options(t, [:unicode, :ucp | acc])
+  defp translate_options(<<?i, t::binary>>, acc), do: translate_options(t, [:caseless | acc])
+  defp translate_options(<<?x, t::binary>>, acc), do: translate_options(t, [:extended | acc])
+  defp translate_options(<<?f, t::binary>>, acc), do: translate_options(t, [:firstline | acc])
+  defp translate_options(<<?U, t::binary>>, acc), do: translate_options(t, [:ungreedy | acc])
+
+  defp translate_options(<<?s, t::binary>>, acc),
+    do: translate_options(t, [:dotall, {:newline, :anycrlf} | acc])
+
+  defp translate_options(<<?m, t::binary>>, acc), do: translate_options(t, [:multiline | acc])
+
+  # TODO: Remove on 2.0
+  defp translate_options(<<?r, t::binary>>, acc) do
+    IO.warn("the /r modifier in regular expressions is deprecated, please use /U instead")
+    translate_options(t, [:ungreedy | acc])
+  end
+
+  defp translate_options(<<>>, acc), do: acc
+  defp translate_options(rest, _acc), do: {:error, rest}
 end
