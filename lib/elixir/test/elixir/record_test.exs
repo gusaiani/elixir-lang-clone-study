21c052c9d84a7656304642eea4fc708c9edaf67f
Gustavo Saiani <gustavo@poe.ma>
2018-10-09 15:47:05 -0300

    Bring RecordTest up-to-date

--- lib/elixir/test/elixir/record_test.exs
+++ lib/elixir/test/elixir/record_test.exs
@@ -161,5 +161,137 @@ defmodule RecordTest do
              binary: <<1, 2, 3>>,
              charlist: 'abc'
            ]
+
+    assert defaults(defaults(), :struct) == ~D[2016-01-01]
+    assert defaults(defaults(), :map) == %{}
+    assert defaults(defaults(), :tuple_zero) == {}
+    assert defaults(defaults(), :tuple_one) == {1}
+    assert defaults(defaults(), :tuple_two) == {1, 2}
+    assert defaults(defaults(), :tuple_three) == {1, 2, 3}
+    assert defaults(defaults(), :list) == [1, 2, 3]
+    assert defaults(defaults(), :call) == MapSet.new()
+    assert defaults(defaults(), :string) == "abc"
+    assert defaults(defaults(), :binary) == <<1, 2, 3>>
+    assert defaults(defaults(), :charlist) == 'abc'
+  end
+
+  test "records with literal defaults and record in a variable" do
+    defaults = defaults()
+
+    assert defaults(defaults) == [
+             struct: ~D[2016-01-01],
+             map: %{},
+             tuple_zero: {},
+             tuple_one: {1},
+             tuple_two: {1, 2},
+             tuple_three: {1, 2, 3},
+             list: [1, 2, 3],
+             call: MapSet.new(),
+             string: "abc",
+             binary: <<1, 2, 3>>,
+             charlist: 'abc'
+           ]
+
+    assert defaults(defaults, :struct) == ~D[2016-01-01]
+    assert defaults(defaults, :map) == %{}
+    assert defaults(defaults, :tuple_zero) == {}
+    assert defaults(defaults, :tuple_one) == {1}
+    assert defaults(defaults, :tuple_two) == {1, 2}
+    assert defaults(defaults, :tuple_three) == {1, 2, 3}
+    assert defaults(defaults, :list) == [1, 2, 3]
+    assert defaults(defaults, :call) == MapSet.new()
+    assert defaults(defaults, :string) == "abc"
+    assert defaults(defaults, :binary) == <<1, 2, 3>>
+    assert defaults(defaults, :charlist) == 'abc'
+  end
+
+  test "records with dynamic arguments" do
+    record = file_info()
+    assert file_info(record, :size) == :undefined
+
+    record = user()
+    assert user(record) == [name: "john", age: 25]
+    assert user(user()) == [name: "john", age: 25]
+
+    msg =
+      "expected argument to be a literal atom, literal keyword or a :file_info record, " <>
+        "got runtime: {RecordTest, \"john\", 25}"
+
+    assert_raise ArgumentError, msg, fn ->
+      file_info(record)
+    end
+
+    pretender = {RecordTest, "john"}
+
+    msg =
+      "expected argument to be a RecordTest record with 2 fields, " <>
+        "got: {RecordTest, \"john\"}"
+
+    assert_raise ArgumentError, msg, fn ->
+      user(pretender)
+    end
+
+    pretender = {RecordTest, "john", 25, []}
+
+    msg =
+      "expected argument to be a RecordTest record with 2 fields, " <>
+        "got: {RecordTest, \"john\", 25, []}"
+
+    assert_raise ArgumentError, msg, fn ->
+      user(pretender)
+    end
+  end
+
+  test "records visibility" do
+    assert macro_exported?(__MODULE__, :user, 0)
+    refute macro_exported?(__MODULE__, :file_info, 1)
+  end
+
+  test "records with no defaults" do
+    record = timestamp()
+    assert timestamp(record, :date) == nil
+    assert timestamp(record, :time) == nil
+
+    record = timestamp(date: :foo, time: :bar)
+    assert timestamp(record, :date) == :foo
+    assert timestamp(record, :time) == :bar
+  end
+
+  test "records defined multiple times" do
+    msg = "cannot define record :r because a definition r/0 already exists"
+
+    assert_raise ArgumentError, msg, fn ->
+      defmodule M do
+        import Record
+        defrecord :r, [:a]
+        defrecord :r, [:a]
+      end
+    end
+  end
+
+  test "macro and record with the same name defined" do
+    msg = "cannot define record :a because a definition a/1 already exists"
+
+    assert_raise ArgumentError, msg, fn ->
+      defmodule M do
+        defmacro a(_) do
+        end
+
+        require Record
+        Record.defrecord(:a, [:a])
+      end
+    end
+
+    msg = "cannot define record :a because a definition a/2 already exists"
+
+    assert_raise ArgumentError, msg, fn ->
+      defmodule M do
+        defmacro a(_, _) do
+        end
+
+        require Record
+        Record.defrecord(:a, [:a])
+      end
+    end
   end
 end
