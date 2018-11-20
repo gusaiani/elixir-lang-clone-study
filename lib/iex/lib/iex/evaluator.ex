af4a5856e0439871361c871273e06b21631ecdc1
Gustavo Saiani <gustavo@poe.ma>
2018-11-20 11:09:34 -0200

    Start IEx.Evaluator

---
+++ lib/iex/lib/iex/evaluator.ex
@@ -0,0 +1,16 @@
+defmodule IEx.Evaluator do
+  @moduledoc false
+
+  @doc """
+  Eval loop for an IEx session. Its responsibilities include:
+
+    * loading of .iex files
+    * evaluating code
+    * trapping exceptions in the code being evaluated
+    * keeping expression history
+
+  """
+  def init(command, server, leader, opts) do
+    old_leader = Process.group_leader
+  end
+end
