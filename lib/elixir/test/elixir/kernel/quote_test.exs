033f6694092e95708bfc746f5261783b777317f9
gusaiani <gustavo@poe.ma>
2020-09-21 09:53:37 -0300

    Continue quote_test

--- lib/elixir/test/elixir/kernel/quote_test.exs
+++ lib/elixir/test/elixir/kernel/quote_test.exs
@@ -29,4 +29,38 @@ defmodule Kernel.QuoteTest do
     assert quote(line: false, do: bar(1, 2, 3)) == {:bar, [], [1, 2, 3]}
     assert quote(line: true, do: bar(1, 2, 3)) == {:bar, [line: __ENV__.line], [1, 2, 3]}
   end
+
+  test "quote line var" do
+    line = __ENV__.line
+    assert quote(line: line, do: bar(1, 2, 3)) == {:bar, [line: line], [1, 2, 3]}
+
+    assert_raise ArgumentError, fn ->
+      line = "oops"
+      quote(line: line, do: bar(1, 2, 3))
+    end
+
+    assert_raise ArgumentError, fn ->
+      line = true
+      quote(line: line, do: bar(1, 2, 3))
+    end
+  end
+
+  test "quote context var" do
+    context = :dynamic
+    assert quote(context: context, do: bar) == {:bar, [], :dynamic}
+
+    assert_raise ArgumentError, fn ->
+      context = "oops"
+      quote(context: context, do: bar)
+    end
+
+    assert_raise ArgumentError, fn ->
+      context = nil
+      quote(context: context, do: bar)
+    end
+  end
+
+  test "operator precedence" do
+    assert {:+, _, [{:+, _, [1, _]}, 1]} = quote(do: 1 + Foo.l() + 1)
+  end
 end
