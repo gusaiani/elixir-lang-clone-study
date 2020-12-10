517aa40daf610207b72c0483f80b79c218e3ac0a
Gustavo Saiani <gustavo@poe.ma>
2020-12-10 08:42:55 -0300

    More of ExUnit.CaptureIo

--- lib/ex_unit/lib/ex_unit/capture_io.ex
+++ lib/ex_unit/lib/ex_unit/capture_io.ex
@@ -43,12 +43,12 @@ defmodule ExUnit.CaptureIO do
 
   ## IO devices
 
-  You may capture the IO from any registered  IO device. The device name given
+  You may capture the IO from any registered IO device. The device name given
   must be an atom representing the name of a registered process. In addition,
   Elixir provides two shortcuts:
 
     * `:stdio` - a shortcut for `:standard_io`, which maps to
-      the current `Process.group_leader/0` in Erlang.
+      the current `Process.group_leader/0` in Erlang
 
     * `:stderr` - a shortcut for the named process `:standard_error`
       provided in Erlang
@@ -79,9 +79,9 @@ defmodule ExUnit.CaptureIO do
       ...> end) == "> this is input"
       true
 
-      iex> capture_io([input: "this is input", capture_promps: false], fn ->
+      iex> capture_io([input: "this is input", capture_prompt: false], fn ->
       ...>   input = IO.gets("> ")
-      ...> IO.write(input)
+      ...>   IO.write(input)
       ...> end) == "this is input"
       true
 
@@ -91,5 +91,56 @@ defmodule ExUnit.CaptureIO do
   If you want to also capture the result of the function executed inside
   the `capture_io`, you can use `Kernel.send/2` to send yourself a message
   and use `ExUnit.Assertions.assert_received/2` to match on the results:
+
+      capture_io([input: "this is input", capture_prompt: false], fn ->
+        send(self(), {:block_result, 42})
+        # ...
+      end)
+
+      assert_received {:block_result, 42}
+
   """
+  @spec capture_io((() -> any())) :: String.t()
+  def capture_io(fun) when is_function(fun, 0) do
+    capture_io(:stdio, [], fun)
+  end
+
+  @spec capture_io(atom(), (() -> any())) :: String.t()
+  def capture_io(device, fun) when is_atom(device) and is_function(fun, 0) do
+    capture_io(device, [], fun)
+  end
+
+  @spec capture_io(String.t(), (() -> any())) :: String.t()
+  def capture_io(input, fun) when is_binary(input) and is_function(fun, 0) do
+    capture_io(:stdio, [input: input], fun)
+  end
+
+  @spec capture_io(keyword(), (() -> any())) :: String.t()
+  def capture_io(options, fun) when is_list(options) and is_function(fun, 0) do
+    capture_io(:stdio, options, fun)
+  end
+
+  @spec capture_io(atom(), String.t(), (() -> any())) :: String.t()
+  def capture_io(device, input, fun)
+      when is_atom(device) and is_binary(input) and is_function(fun, 0) do
+    capture_io(device, [input: input], fun)
+  end
+
+  @spec capture_io(atom(), keyword(), (() -> any))) :: String.t()
+  def capture_io(device, options, fun)
+      when is_atom(device) and is_list(options) and is_function(fun, 0) do
+    do_capture_io(map_dev(device), options, fun)
+  end
+
+  defp map_dev(:stdio), do: :standard_io
+  defp map_dev(:stderr), do: :standard_error
+  defp map_dev(other), do: other
+
+  defp do_capture_io(:standard_io, options, fun) do
+    prompt_config = Keyword.get(options, :capture_prompt, true)
+    encoding = Keyword.get(options, :encoding, :unicode)
+    input = Keyword.get(options, :input, "")
+
+    original_gl = Process.group_leader()
+  end
 end
